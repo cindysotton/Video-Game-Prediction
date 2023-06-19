4e7ab5fa263c615e4b3b4e02549b0a5e11c0daa4
cindysotton <131467569+cindysotton@users.noreply.github.com>
2023-06-19 17:42:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -156,6 +156,7 @@ if selected == "Méthodologie":
     with tab1:
         st.markdown("""
 **Nos données - Dataset Vgchart:**
+
 Le dataset Vgchart est le jeu initial fourni dans le cadre du projet disponible sur Kaggle. les variables se répartissent comme suit:
 * Nom
 * Date de sortie
@@ -165,6 +166,7 @@ Le dataset Vgchart est le jeu initial fourni dans le cadre du projet disponible 
 * Ventes par région
 
 **Complétion des données - Utilisation du scrapping:**
+
 Nous avons scrappé le site Vgchart pour récupérer : 
 * des données plus récentes
 * les variables Critic Score et Studio. 
@@ -172,6 +174,7 @@ Nous avons scrappé le site Vgchart pour récupérer :
 Ce nouveau jeu annule et remplace le premier jeu de données de Vgchart.
 
 Pour compléter les données à disposition nous avons scrappé le site Metacritic et le site jeuvidéo.com avec les librairies Sélenium et BeautifulSoup pour collecter les notes utilisateurs et presse sur une communauté orientée mondiale et française.
+
 Le scrapping a permis de récupérer les informations suivantes:
 * Description
 * Studio
@@ -180,6 +183,7 @@ Le scrapping a permis de récupérer les informations suivantes:
 * Note presse
 * Nombre de vote presse
 * Genre
+
 Les résultats n'étant pas à la hauteur autant pour Metacritic que pour jeuxvideo.com (% de Nan trop élevé) nous n'avons pas conservé les données. Pour jeuxvideo.com, les données avaient encore plus de Nan. En effet, les jeux scrappés avec un id français ne pouvaient communiquer avec le dataset initial.""")
             
         st.markdown ('Voici notre nouveau dataset:') 
